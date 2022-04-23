b545388735d8672cd4d7b572c8cff53fb508793c
Andrej Vyšný <92629581+andrejvysny@users.noreply.github.com>
2022-04-23 21:51:38 +0200

    Update settings.py

--- services/services/settings.py
+++ services/services/settings.py
@@ -58,6 +58,14 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'services.urls'
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "*"
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
